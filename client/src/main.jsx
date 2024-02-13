2d386b18ec2d82db40c0d4efe393c2434118a5fa
harsh <harshdana1@gmail.com>
2024-02-13 14:28:19 +0530

    update themeprovider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import { store, persistor } from './redux/store'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import ThemeProvider from './components/themeProvider.jsx'
+import ThemeProvider from '../src/components/ThemeProvider.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
